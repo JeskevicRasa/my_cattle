81eeac110449794e49228f978122d6283fdc1644
Beata Savkaite <b.savkaite@gmail.com>
2023-04-27 19:21:49 +0300

    Added cattle_info page

--- my_farm/urls.py
+++ my_farm/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, cattle_info
 
 app_name = "my_farm"
 
 urlpatterns = [
     path('', home, name='home'),
+    path('cattle_info/', cattle_info, name='cattle_info'),
 
 ]
--- my_farm/views.py
+++ my_farm/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, you are in my farm yeay")
+
+
+def cattle_info(request):
+    return HttpResponse("Hello, here is going to be all cattle info")

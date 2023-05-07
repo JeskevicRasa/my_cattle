3fe1b29d232385431db386066c9d336d64a2fa36
JeskevicRasa <82932245+JeskevicRasa@users.noreply.github.com>
2023-05-07 21:28:51 +0300

    Table cattle info is added with possibility add info to database and delete it.

--- my_farm/views.py
+++ my_farm/views.py
@@ -9,9 +9,41 @@ def home(request):
     return HttpResponse("Hello, you are in my farm yeay")
 
 
+# def cattle_info(request):
+#     rows = Cattle.objects.all()
+#     return render(request, 'my_farm/cattle_info.html', {'rows': rows})
+
 def cattle_info(request):
-    rows = Cattle.objects.all()
-    return render(request, 'my_farm/cattle_info.html', {'rows': rows})
+    cattle = Cattle.objects.all()
+
+    # Get a list of all the columns in the Cattle model
+    all_columns = [f.name for f in Cattle._meta.get_fields()]
+
+    if request.method == 'POST':
+        selected_columns = request.POST.getlist('columns')
+
+        if len(selected_columns) > 0:
+            # Build a dictionary of column names and their corresponding database field names
+            column_dict = {'ID': 'id', 'Type': 'type', 'Number': 'number', 'Name': 'name', 'Gender': 'gender',
+                           'Breed': 'breed', 'Birth Date': 'birth_date', 'Entry date': 'entry_date', 'End date': 'end_date',
+                           'Comments': 'comments'}
+
+            # Build a list of the selected database field names
+            selected_fields = [column_dict[column] for column in selected_columns]
+
+            # Filter the queryset to only include the selected fields
+            cattle = cattle.values(*selected_fields)
+        else:
+            # If no columns were selected, display an error message
+            error_message = 'Please select at least one column to display.'
+            return render(request, 'my_farm/cattle_info.html',
+                          {'cattle': cattle, 'columns': all_columns, 'error_message': error_message})
+
+    # If the form has not been submitted, display all columns by default
+    else:
+        cattle = cattle.values()
+
+    return render(request, 'my_farm/cattle_info.html', {'cattle': cattle, 'columns': all_columns})
 
 
 def add_row(request):
